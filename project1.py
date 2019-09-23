acd132e67a109c0a28f4c2db08f77b9e2fae0472
Abdeslam <ia_benameur@esi.dz>
2019-09-23 06:54:51 +0100

    Main Game Loop

--- project1.py
+++ project1.py
@@ -6,6 +6,9 @@ window.bgcolor("black")
 window.setup(height=600, width=800)
 window.tracer(0)  # it stops the window from updating to speed up the game quiet a bit
 
+# Score
+score_a = score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -26,13 +29,22 @@ paddle_b.goto(350, 0)
 
 # Ball
 ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("square")
+ball.speed("slow")
+ball.shape("circle")
 ball.color("white")
 ball.penup()  # to avoid drawing the path of moving objects
 ball.goto(0, 0)
 ball.dx = 2
-ball.dy = 2
+ball.dy = -2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 # Function
 def paddle_a_up():
@@ -55,9 +67,45 @@ def paddle_b_down():
     y -= 20
     paddle_b.sety(y)
 
+
 # Keyboard binding
 window.listen()
 window.onkeypress(paddle_a_up, "z")
 window.onkeypress(paddle_a_down, "s")
 window.onkeypress(paddle_b_up, "Up")
 window.onkeypress(paddle_b_down, "Down")
+
+
+# Main game loop
+while True:
+    window.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle and ball collisions
+    if 340 < ball.xcor() < 350 and paddle_b.ycor() - 40 < ball.ycor() < paddle_b.ycor() + 40:
+        ball.setx(340)
+        ball.dx *= -1
+
+    if -350 < ball.xcor() < -340 and (paddle_a.ycor() - 40 < ball.ycor() < paddle_a.ycor() + 40):
+        ball.setx(-340)
+        ball.dx *= -1
