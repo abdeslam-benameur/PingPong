203ddbb5ed0526e8699815e12b7742cd09b504a7
Abdeslam <ia_benameur@esi.dz>
2019-09-24 19:43:48 +0100

    some improvements

--- project1.py
+++ project1.py
@@ -1,4 +1,5 @@
 import turtle
+import os
 
 window = turtle.Screen()
 window.title("Ping Pong Game")
@@ -31,20 +32,22 @@ paddle_b.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed("slow")
 ball.shape("circle")
-ball.color("white")
-ball.penup()  # to avoid drawing the path of moving objects
+ball.color("blue")
+ball.pendown()  # to avoid drawing the path of moving objects*
+ball.pencolor("black")
 ball.goto(0, 0)
 ball.dx = 2
-ball.dy = -2
+ball.dy = 2
 
 # Pen
 pen = turtle.Turtle()
 pen.speed(0)
-pen.color("white")
+pen.color("yellow")
 pen.penup()
 pen.hideturtle()
 pen.goto(0,260)
 pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 
 # Function
 def paddle_a_up():
@@ -52,15 +55,18 @@ def paddle_a_up():
     y += 20
     paddle_a.sety(y)
 
+
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
 
+
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
     paddle_b.sety(y)
+
 
 def paddle_b_down():
     y = paddle_b.ycor()
@@ -88,18 +94,26 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        # os.system("aplay bounce.wav &")
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        # os.system("aplay bounce.wav &")
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle and ball collisions
     if 340 < ball.xcor() < 350 and paddle_b.ycor() - 40 < ball.ycor() < paddle_b.ycor() + 40:
